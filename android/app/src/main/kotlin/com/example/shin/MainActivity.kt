79cc996ec9999a1b91e95b046eada2d49d3da451
abdualrahmankobicy <98434120+abdualrahmankobicy@users.noreply.github.com>
2023-11-28 03:12:24 +0300

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/shin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+int x = 5;
+void main() {
+  print(x);
+}
